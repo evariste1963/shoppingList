6da8daa29c240859887f097cfd3c0d7de9a29942
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-03-09 09:03:11 +0000

    delete doneg -- just refactoring to do

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const container = document.getElementsByClassName(".container");
-const shopping = document.getElementsByClassName(".shopping");
 const input = document.getElementById("userInput");
 const ul = document.querySelector("ul");
-const field = document.getElementById("field");
 const quill = document.getElementById("quill");
 const delBtn = document.getElementById("delBtn");
-const allLi = document.getElementsByTagName("li");
 
-function inputLength() {
+/*function inputLength() {
   return input.value.length;
-}
+}*/
 
 //animate quill on userInput focus
 input.addEventListener("focus", function () {
@@ -35,13 +31,11 @@ function createListElement() {
   }
 }
 
-//THIS DOES NOT WORK BECAUSE I IS INCREMENTING PAST ALLLI.LENGTH -- use forEach somehow
+//delete all done items
 const delDone = function () {
-  for (let i = 0; i <= allLi.length; i++) {
-    if (allLi[i].classList.contains("done")) {
-      allLi[i].remove();
-      console.log(allLi.length);
-    }
+  const elements = document.getElementsByClassName("done");
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
   }
 };
 
@@ -54,60 +48,3 @@ userInput.addEventListener("keydown", function (e) {
     createListElement();
   }
 });
-
-//li.addEventListener("click", function () {});
-
-//function createListItem(){
-// Create li element, style it and append it to ul
-// Create delete button, style it and append it to li
-// Hide empty list text if li's exist
-// Toggle line-through on click
-// Delete item
-//}
-
-//1. 1. If you click on the list item, it toggles the .done  class on and off.
-
-//2. Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
-
-//3. BONUS: When adding a new list item, it automatically adds the delete button next to it (hint: be sure to check if new items are clickable too!)
-
-/*
-function createListItem() {
-  // Create li element, style it and append it to ul
-  var li = document.createElement('li');
-  li.classList.add('listItem');
-  li.append(document.createTextNode(input.value));
-  li.addEventListener('click', toggleDone);
-  ul.appendChild(li);
-  input.value = "";
-  
-  // Create delete button, style it and append it to li
-  var delBtn = document.createElement('button');
-  var delX = document.createElement('i');
-  delX.classList.add('fas', 'fa-times');
-  delBtn.classList.add('custBtn');
-  delBtn.appendChild(delX);
-  delBtn.addEventListener('click', delItem);
-  li.append(delBtn);
-  
-  // Hide empty list text if li's exist
-  if(checkLis() > 0) {
-    var emptyList = document.querySelector('h2');
-    emptyList.style.display = "none";
-  }
-  
-  // Toggle line-through on click
-  function toggleDone() {
-    li.classList.toggle('done');
-  }
-  
-  // Delete item
-  function delItem() {
-    li.remove();
-    if(checkLis() == 0) {
-      var emptyList = document.querySelector('h2');
-      emptyList.style.display = "block";
-    }
-  }
-}
-*/
