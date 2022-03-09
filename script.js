b4dde4606e01c8e0ec7316ab668e8e041e9af640
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-03-09 10:51:45 +0000

    @media added

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const ul = document.querySelector("ul");
 const quill = document.getElementById("quill");
 const delBtn = document.getElementById("delBtn");
 
-input.addEventListener("keydown", function () {
-  quill.classList.add("spin");
-});
-
 function createListElement() {
   if (userInput.value.length > 0) {
     //create li element & append List ul
@@ -15,12 +11,13 @@ function createListElement() {
     list.addEventListener("click", toggleDone);
     ul.appendChild(list);
 
-    //empty input field & remove focus
+    //empty inputField & remove focus
     input.value = "";
 
     //remove quill spin animation
     quill.classList.remove("spin");
 
+    //toggle text strikethrough on item focus -- this should be refactored if possible
     function toggleDone() {
       list.classList.toggle("done");
     }
@@ -29,18 +26,25 @@ function createListElement() {
 
 //delete all done items
 const delDone = function () {
-  const elements = document.getElementsByClassName("done");
-  while (elements.length > 0) {
-    elements[0].parentNode.removeChild(elements[0]);
+  const items = document.getElementsByClassName("done");
+  while (items.length > 0) {
+    items[0].parentNode.removeChild(items[0]);
   }
 };
 
-delBtn.addEventListener("click", function () {
-  delDone();
+//animate quill while typing
+input.addEventListener("keydown", function () {
+  quill.classList.add("spin");
 });
 
+//add input filed to list on enter
 userInput.addEventListener("keydown", function (e) {
   if (e.key == "Enter") {
     createListElement();
   }
+});
+
+//call delete marked items from list
+delBtn.addEventListener("click", function () {
+  delDone();
 });
